61387a96657a42db1528056ee97cad330fc0b0c7
JorgeAgulloM <agullojorge@gmail.com>
2023-10-19 16:44:51 +0200

    New test web (link Bio)

---
+++ env_openai/keys.py
@@ -0,0 +1,1 @@
+API_KEY = "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"
--- web_reflex_py/web_reflex_py.py
+++ web_reflex_py/web_reflex_py.py
@@ -1,53 +1,15 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
 import reflex as rx
 
-from web_reflex_py import style
-from web_reflex_py.state import State
+class State(rx.State):
+    """The app state."""
 
-def qa(question: str, answer: str) -> rx.Component:
-    return rx.box(
-        rx.box(
-            rx.text(question, text_align="right", color="black"), 
-            style=style.question_style
-        ),
-        rx.box(
-            rx.text(answer, text_align="left", color="black"), 
-            style=style.answer_style
-        ),
-        margin_y="1em",
-    )
-
-
-def chat() -> rx.Component:
-    return rx.box(
-        rx.foreach(
-            State.chat_history,
-            lambda messages: qa(messages[0], messages[1]),
-        )
-    )
-
-
-def action_bar() -> rx.Component:
-    return rx.hstack(
-        rx.input(
-            value=State.question,
-            placeholder="Ask a question",
-            on_change=State.set_question, 
-            style=style.input_style,
-        ),
-        rx.button(
-            "Ask",
-            on_click=State.answer, 
-            style=style.button_style,
-            color="black",
-        ),
-    )
+    pass
 
 
 def index() -> rx.Component:
-    return rx.container(
-            chat(),
-            action_bar()
-        )
+    return rx.text("Hola Reflex!", color="blue")
+
 
 # Add state and page to the app.
 app = rx.App()
