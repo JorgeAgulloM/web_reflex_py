b2aaf1f9c94923227cf698550caf36c8c74d5005
JorgeAgulloM <agullojorge@gmail.com>
2023-10-19 20:38:59 +0200

    Update

--- web_reflex_py/views/links/links.py
+++ web_reflex_py/views/links/links.py
@@ -1,32 +1,35 @@
 import reflex as rx
-from web_reflex_py.components.link_button import link_buttun
+from web_reflex_py.components.link_button import link_button
+from web_reflex_py.components.title import title
 
 def links() -> rx.Component:
     return rx.vstack(
-        link_buttun(
-            'Click Me!', 
-            'recargar pagina', 
-            'http://localhost:3000/'
+        title("Personal"),
+        link_button(
+            'Github', 
+            'Con esto conocerás mi potencial', 
+            'https://github.com/JorgeAgulloM'
         ),
-        link_buttun(
-            'Twitch', 
-            '¿Cuando toca Mouredev?', 
-            'https://www.twitch.tv/super_yorch'
-        ),
-        link_buttun(
+        link_button(
             'Linkedin', 
             'Para que pueda ojear mi perfil', 
             'https://www.linkedin.com/in/jorgeagullo/'
         ),
-        link_buttun(
+        title("Cosas varias"),
+        link_button(
+            'Twitch', 
+            '¿Cuando toca Mouredev?', 
+            'https://www.twitch.tv/super_yorch'
+        ),
+        link_button(
             'Youtube', 
             'Los vídos de mis apps',
             'https://www.youtube.com/channel/UCcox318Jjfm8H6Roesi3SZg'
         ),
-        link_buttun(
-            'Github', 
-            'Con esto conocerás mi potencial', 
-            'https://github.com/JorgeAgulloM'
+        link_button(
+            'Click Me!', 
+            'recargar pagina', 
+            'http://localhost:3000/'
         ),
         width="100%"
     )
