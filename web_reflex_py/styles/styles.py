f23eeef0a6b0db694e9393caa8bb1fe752eb93bf
JorgeAgulloM <agullojorge@gmail.com>
2023-10-28 19:15:35 +0200

    Update stiles

--- web_reflex_py/styles/styles.py
+++ web_reflex_py/styles/styles.py
@@ -1,25 +1,34 @@
 import reflex as rx
 from enum import Enum
+from .colors import Color, TextColor
 
 #Constants
 MAX_WIDTH = "560px"
 
 #Sizes
 class Size(Enum):
+    ZERO = "0px !importat"
     SMALL = "0.5em"
     MEDIUM = "0.8em"
     DEFAULT = "1em"
+    LARGE = "1.5em"
     BIG = "2em"
 
 
 # Styles
 BASE_STYLES = {
+    "background_color" : Color.BACKGROUND.value,
     rx.Button: {
         "width" : "100%",
-        "heigth" : "100%",
+        "height" : "100%",
         "display": "block",
         "padding": Size.SMALL.value,
-        "border_radius": Size.DEFAULT.value
+        "border_radius": Size.DEFAULT.value,
+        "color": TextColor.HEADER.value,
+        "background_color": Color.CONTENT.value,
+        "_hover": {
+            "background_color": Color.SECONDARY.value,
+        },
     },
     rx.Link: {
         "text_decoration": "none",
@@ -27,15 +36,24 @@ BASE_STYLES = {
     }
 }
 
+navbar_title_style = dict(
+    font_family="Outfit",
+    font_size=Size.LARGE.value,
+    font_weight="semi-bold"
+)
+
 title_style = dict(
     width="100%",
-    padding_top=Size.DEFAULT.value
+    padding_top=Size.DEFAULT.value,
+    color=TextColor.HEADER.value
 )
 
 button_title_style = dict(
-    font_size=Size.DEFAULT.value
+    font_size=Size.DEFAULT.value,
+    color=TextColor.HEADER.value
 )
 
 button_body_style = dict(
-    font_size=Size.MEDIUM.value
+    font_size=Size.MEDIUM.value,
+    color=TextColor.BODY.value
 )
